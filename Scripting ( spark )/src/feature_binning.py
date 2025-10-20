d37e507f169f55028371d8ff718821502c371196
Dinod <dinodimanjith404@gmail.com>
2025-10-20 18:25:11 +0530

    Pyspark initialization

---
+++ Scripting ( spark )/src/feature_binning.py
@@ -0,0 +1,35 @@
+import logging
+import pandas as pd
+from abc import ABC,abstractmethod
+logging.basicConfig(level=logging.INFO, format=
+    '%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+class FeatureBinningStrategy(ABC):
+    @abstractmethod
+    def bin_feature(self,df:pd.DataFrame,column:str)->pd.DataFrame:
+        pass
+
+class CustomBinningStrategy(FeatureBinningStrategy):
+    def __init__(self,bin_definition):
+       self.bin_definition = bin_definition
+
+    def bin_feature(self, df,column):
+        def assign_value(value):
+            if value == 850:
+                return "Excellent"
+            
+            for bin_label,bin_range in self.bin_definition.items():
+                if len(bin_range)==2:
+                    if(bin_range[0]) <= value <=bin_range[1]:
+                        return bin_label
+                elif len(bin_range) == 1:
+                    if value >= bin_range[0]:
+                            return bin_label
+            if value>850:
+                return "Invalid"
+        
+        df[f"{column}Bins"] = df[column].apply(assign_value)
+        del df[column]
+
+        return df
---
+++ Scripting ( spark )/src/outlier_detection.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import logging
+from abc import ABC,abstractmethod
+logging.basicConfig(level=logging.INFO, format=
+    '%(asctime)s - %(levelname)s - %(message)s')
+
+
+class OutlierDetectionStrategy(ABC):
+    def detect_coutliers(self,df:pd.DataFrame,columns: list) -> pd.DataFrame:
+        pass
+
+class IQROutlierDetection(OutlierDetectionStrategy):
+    def detect_coutliers(self,df,columns):
+        outliers = pd.DataFrame(False,index=df.index,columns=columns)
+
+        for col in columns:
+            df[col] = df[col].astype(float)
+            Q1 = df[col].quantile(0.25)
+            Q3 = df[col].quantile(0.75)
+            IQR = Q3 - Q1 
+
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+            outliers[col] = (df[col] < lower_bound) | (df[col] > upper_bound)
+        
+        return outliers
+
+class OutlierDetector:
+    def __init__(self,strategy):
+        self.strategy=strategy
+    
+    def detect_coutliers(self,df,selected_columns):
+        return self.strategy.detect_coutliers(df,selected_columns)
+    
+    def handle_outliers(self,df,selected_columns):
+        outliers = self.detect_coutliers(df,selected_columns)
+        outliers_count = outliers.sum(axis=1)
+        rows_to_remove = outliers_count >=2
+        return df[~rows_to_remove]
+
